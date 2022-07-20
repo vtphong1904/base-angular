86f200a7073b81a928cec2c5011ad5a13f628918
unknown <vuthanhphong190499@gmail.com>
2022-07-20 14:57:43 +0700

    set item for header

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -22,6 +22,7 @@ export class AuthInterceptor implements HttpInterceptor {
     // if (this._authService.accessToken) {
     //   newReq = req.clone({
     //     headers: req.headers.set('Authorization', 'Bearer ' + this._authService.accessToken)
+    //     .set('ProjectId', localStorage.getItem('projectId') ?? ''),
     //   });
     // }
 
